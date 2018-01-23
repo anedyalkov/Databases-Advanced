75b232c047d1fcede674cc10798d52217e460b58
anedyalkov <nedyalkov78@gmail.com>
2018-01-23 09:33:03 +0200

    Entity Relations

---
+++ 04.Entity Relations/P03_FootballBetting/P03_FootballBetting.Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace P03_FootballBetting.Data
+{
+
+    class Configuration
+    {
+        internal static string ConnectionString { get; set; } = @"Server=(localdb)\MSSQLLocalDB;Database=FootballBetting;Integrated Security=True";
+    }
+}
---
+++ 04.Entity Relations/P03_FootballBetting/P03_FootballBetting/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace P03_FootballBetting
+{
+    using System;
+    using P03_FootballBetting.Data;
+    using P03_FootballBetting.Data.Models;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new FootballBettingContext())
+            {
+                db.Database.EnsureCreated();
+            }
+        }
+    }
+}
